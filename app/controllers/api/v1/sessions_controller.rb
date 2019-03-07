51574eb000e10af38aabcd5272f75353f1f326b1
Yuta Toyokawa <kppg42@gmail.com>
2019-03-07 22:46:34 +0900

    add get authenticate request

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,5 +1,9 @@
 class Api::V1::SessionsController < Api::V1::ApplicationController
-  skip_before_action :authorize_request
+  skip_before_action :authorize_request, only: :create
+
+  def new
+    render json: "ok"
+  end
 
   def create
     user = User.find_by(email: params[:email])
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :users, only: :index
+      get  :sign_in, to: "sessions#new"
       post :sign_in, to: "sessions#create"
     end
   end
