a6437301bc3163f9570650ae2e48a26bbff0e113
Yuta Toyokawa <kppg42@gmail.com>
2019-03-11 23:49:02 +0900

    add sort words logic

--- app/controllers/api/v1/users/words_controller.rb
+++ app/controllers/api/v1/users/words_controller.rb
@@ -2,17 +2,17 @@ class Api::V1::Users::WordsController < Api::V1::ApplicationController
   before_action :set_user
   
   def index
-    render json: @user.words
+    render json: @user.words.order(created_at: :desc)
   end
   
   def create
     @user.words.create!(word_params)
-    render json: @user.words
+    render json: @user.words.order(created_at: :desc)
   end
   
   def destroy
     @user.words.find(params[:id]).destroy!
-    render json: @user.words
+    render json: @user.words.order(created_at: :desc)
   end
   
   private
