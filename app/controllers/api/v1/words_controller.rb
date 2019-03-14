1f3b7e89cdd7c429703903301e5442f579a5f076
Yuta Toyokawa <kppg42@gmail.com>
2019-03-15 00:25:40 +0900

    add word update action

--- app/controllers/api/v1/words_controller.rb
+++ app/controllers/api/v1/words_controller.rb
@@ -1,6 +1,24 @@
 class Api::V1::WordsController < Api::V1::ApplicationController
+  before_action :set_word
+  
   def show
-    word = Word.find(params[:id])
-    render json: word.as_json(methods: :knowledges)
+    render json: @word.as_json(methods: :knowledges)
   end
+  
+  def update
+    return render json: { errors: "User can't update." }, status: 403 if current_user != @word.user
+
+    @word.update!(word_params)
+    render json: @word.as_json(methods: :knowledges)
+  end
+  
+  private
+  
+    def set_word
+      @word = Word.find(params[:id])
+    end
+  
+    def word_params
+      params.permit(:text)
+    end
 end
