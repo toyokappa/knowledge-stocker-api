2e542795ca845d94d5fa4278c1a00c54e9382a26
Yuta Toyokawa <kppg42@gmail.com>
2019-03-19 14:38:23 +0900

    fix calc achievement logic

--- app/controllers/api/v1/users/achievements_controller.rb
+++ app/controllers/api/v1/users/achievements_controller.rb
@@ -3,16 +3,16 @@ class Api::V1::Users::AchievementsController < Api::V1::ApplicationController
     user = User.find_by!(name: params[:user_name])
     word_length = user.words.length
     understood_length = user.words.joins(:knowledges).group(:id).length
-    understood_rate = word_length > 0 ? (understood_length.to_d / word_length.to_d) * 100 : 0
+    understood_rate = word_length > 0 ? (understood_length.to_d / word_length.to_d * 100).floor : 0
     knowledge_length = user.knowledges.length
     understandings = user.words.map {|w| w.knowledges.pluck(:understanding).max }
-    ave_understanding = understandings.sum / understandings.length
+    ave_understanding = understandings.length > 0 ? (understandings.sum / understandings.length).floor(2) : "-"
     achievement = {
       wordLength: word_length,
       understoodLength: understood_length,
-      understoodRate: understood_rate.floor,
+      understoodRate: understood_rate,
       knowledgeLength: knowledge_length,
-      aveUnderstanding: ave_understanding.floor(2),
+      aveUnderstanding: ave_understanding,
     }
     render json: achievement
   end
