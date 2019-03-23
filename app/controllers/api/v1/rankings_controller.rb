221a3d5d748b9337994a652921d26c01b2d61cb2
Yuta Toyokawa <kppg42@gmail.com>
2019-03-24 00:42:13 +0900

    fix current user rank

--- app/controllers/api/v1/rankings_controller.rb
+++ app/controllers/api/v1/rankings_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::RankingsController < Api::V1::ApplicationController
   def show
     term = params[:term]
     users = User.rank_as_json(term: term)
-    current_user_rank = users.index {|user| user["name"] == current_user.name }.to_i + 1
+    current_user_rank = users.index {|user| user["name"] == current_user.name }&. + 1
     render json: { users: users.slice(0, 5), currentUserRank: current_user_rank }
   end
 end
