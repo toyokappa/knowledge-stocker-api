2e6a38ec5b61baa06440857d3c41db60ba113a6e
Yuta Toyokawa <kppg42@gmail.com>
2019-04-03 13:32:18 +0900

    add response user image

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -2,13 +2,13 @@ class Api::V1::SessionsController < Api::V1::ApplicationController
   skip_before_action :authorize_request, only: :create
 
   def show
-    render json: { userName: current_user.name }
+    render json: { userName: current_user.name, imageUrl: current_user.image_url }
   end
 
   def create
     user = User.find_or_create_from_auth_params(auth_params)
     token = JsonWebToken.encode(user_id: user.uid)
-    render json: { authToken: token, userName: user.name }
+    render json: { authToken: token, userName: user.name, imageUrl: user.image_url }
   end
 
   private
